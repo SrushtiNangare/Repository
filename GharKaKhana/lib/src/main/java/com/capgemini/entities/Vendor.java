4718aacdb7a0a933d9677211b6a16bb1a0f4452a
kvc12 <48399449+kvc12@users.noreply.github.com>
2021-04-26 11:28:45 +0530

    updated(keval and pratik)

--- GharKaKhana/lib/src/main/java/com/capgemini/entities/Vendor.java
+++ GharKaKhana/lib/src/main/java/com/capgemini/entities/Vendor.java
@@ -1,85 +1,167 @@
 package com.capgemini.entities;
 
+ 
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+ 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+@Component
+@Scope(scopeName = "prototype")
+@Entity
+@Table(name = "VENDOR_TBL")
 public class Vendor {
-	private int vendorId;
-	private String vendorName;
-	private long vendorContact;
-	private String vendorUsername;
-	private String vedorPassword;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int vendorId;
+    
+    @Column(name = "VENDOR_NAME", nullable = false)
+    private String vendorName;
+    
+    @Column(name = "VENDOR_CONTACT", nullable = false)
+    private long vendorContact;
+    
+    @Column(name = "VENDOR_USERNAME", nullable = false)
+    private String vendorUsername;
+    
+    @Column(name = "VENDOR_PASSWORD", nullable = false)
+    private String vedorPassword;
 
-	private Menu foodMenu;
-	private Vendor_Address vendorAddress;
+ 
 
-	public Vendor(int vendorId, String vendorName, long vendorContact, String vendorUsername, String vedorPassword) {
-		super();
-		this.vendorId = vendorId;
-		this.vendorName = vendorName;
-		this.vendorContact = vendorContact;
-		this.vendorUsername = vendorUsername;
-		this.vedorPassword = vedorPassword;
-	}
+    private Menu foodMenu;
+    @Autowired
+    @JoinColumn(name = "VENDOR_ADDRESSID")
+    @OneToOne(cascade = CascadeType.ALL)
+    private Vendor_Address vendorAddress;
 
-	public int getVendorId() {
-		return vendorId;
-	}
+ 
 
-	public void setVendorId(int vendorId) {
-		this.vendorId = vendorId;
-	}
+    
+    
+    public Vendor(Menu foodMenu, Vendor_Address vendorAddress) {
+        super();
+        this.foodMenu = foodMenu;
+        this.vendorAddress = vendorAddress;
+    }
 
-	public String getVendorName() {
-		return vendorName;
-	}
+ 
 
-	public void setVendorName(String vendorName) {
-		this.vendorName = vendorName;
-	}
+    public Vendor(String vendorName, long vendorContact, String vendorUsername, String vedorPassword) {
+        super();
+        this.vendorName = vendorName;
+        this.vendorContact = vendorContact;
+        this.vendorUsername = vendorUsername;
+        this.vedorPassword = vedorPassword;
+    }
 
-	public long getVendorContact() {
-		return vendorContact;
-	}
+ 
 
-	public void setVendorContact(long vendorContact) {
-		this.vendorContact = vendorContact;
-	}
+    //  
+    
+    public int getVendorId() {
+        return vendorId;
+    }
 
-	public String getVendorUsername() {
-		return vendorUsername;
-	}
+ 
 
-	public void setVendorUsername(String vendorUsername) {
-		this.vendorUsername = vendorUsername;
-	}
+    public void setVendorId(int vendorId) {
+        this.vendorId = vendorId;
+    }
 
-	public String getVedorPassword() {
-		return vedorPassword;
-	}
+ 
 
-	public void setVedorPassword(String vedorPassword) {
-		this.vedorPassword = vedorPassword;
-	}
+    public String getVendorName() {
+        return vendorName;
+    }
 
-	public Menu getFoodMenu() {
-		return foodMenu;
-	}
+ 
 
-	public void setFoodMenu(Menu foodMenu) {
-		this.foodMenu = foodMenu;
-	}
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
 
-	public Vendor_Address getVendorAddress() {
-		return vendorAddress;
-	}
+ 
 
-	public void setVendorAddress(Vendor_Address vendorAddress) {
-		this.vendorAddress = vendorAddress;
-	}
+    public long getVendorContact() {
+        return vendorContact;
+    }
 
-	@Override
-	public String toString() {
-		return "Vendor [vendorId=" + vendorId + ", vendorName=" + vendorName + ", vendorContact=" + vendorContact
-				+ ", vendorUsername=" + vendorUsername + ", vedorPassword=" + vedorPassword + ", foodMenu=" + foodMenu
-				+ ", vendorAddress=" + vendorAddress + "]";
-	}
+ 
+
+    public void setVendorContact(long vendorContact) {
+        this.vendorContact = vendorContact;
+    }
+
+ 
+
+    public String getVendorUsername() {
+        return vendorUsername;
+    }
+
+ 
+
+    public void setVendorUsername(String vendorUsername) {
+        this.vendorUsername = vendorUsername;
+    }
+
+ 
+
+    public String getVedorPassword() {
+        return vedorPassword;
+    }
+
+ 
+
+    public void setVedorPassword(String vedorPassword) {
+        this.vedorPassword = vedorPassword;
+    }
+
+ 
+
+    public Menu getFoodMenu() {
+        return foodMenu;
+    }
+
+ 
+
+    public void setFoodMenu(Menu foodMenu) {
+        this.foodMenu = foodMenu;
+    }
+
+ 
+
+    public Vendor_Address getVendorAddress() {
+        return vendorAddress;
+    }
+
+ 
+
+    public void setVendorAddress(Vendor_Address vendorAddress) {
+        this.vendorAddress = vendorAddress;
+    }
+
+ 
+
+    @Override
+    public String toString() {
+        return "Vendor [vendorId=" + vendorId + ", vendorName=" + vendorName + ", vendorContact=" + vendorContact
+                + ", vendorUsername=" + vendorUsername + ", vedorPassword=" + vedorPassword + ", foodMenu=" + foodMenu
+                + ", vendorAddress=" + vendorAddress + "]";
+    }
+
+ 
 
 }
+ 
--- GharKaKhana/lib/src/main/java/com/capgemini/entities/Vendor_Address.java
+++ GharKaKhana/lib/src/main/java/com/capgemini/entities/Vendor_Address.java
@@ -1,59 +1,115 @@
 package com.capgemini.entities;
 
+ 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+ 
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+ 
+
+@Component
+@Scope(scopeName = "prototype")
+@Entity
+@Table(name = "VENDOR_ADDRESS_TBL")
 public class Vendor_Address {
-	private int vendorAddressId;
-	private String vendorCity;
-	private String vendorState;
-	private long vendorPincode;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "VENDOR_ADDRESSID")
+    private int vendorAddressId;
+    
+    @Column(name = "VENDOR_CITY", nullable = false)
+    private String vendorCity;
+    
+    @Column(name = "VENDOR_STATE", nullable = false)
+    private String vendorState;
+    
+    @Column(name = "VENDOR_PINCODE", nullable = false, length = 6)
+    private long vendorPincode;
+   
+    
+    // @onetoone (bi directional  )
+    
+    
+    public Vendor_Address(String vendorCity, String vendorState, long vendorPincode) {
+        super();
+        this.vendorCity = vendorCity;
+        this.vendorState = vendorState;
+        this.vendorPincode = vendorPincode;
+    }
 
-	public Vendor_Address(int vendorAddressId, String vendorCity, String vendorState, long vendorPincode) {
-		super();
-		this.vendorAddressId = vendorAddressId;
-		this.vendorCity = vendorCity;
-		this.vendorState = vendorState;
-		this.vendorPincode = vendorPincode;
-	}
+ 
 
-	public Vendor_Address() {
+    public Vendor_Address() {
 
-	}
+ 
 
-	public int getVendorAddressId() {
-		return vendorAddressId;
-	}
+    }
 
-	public void setVendorAddressId(int vendorAddressId) {
-		this.vendorAddressId = vendorAddressId;
-	}
+ 
 
-	public String getVendorCity() {
-		return vendorCity;
-	}
+    public int getVendorAddressId() {
+        return vendorAddressId;
+    }
 
-	public void setVendorCity(String vendorCity) {
-		this.vendorCity = vendorCity;
-	}
+ 
 
-	public String getVendorState() {
-		return vendorState;
-	}
+    public void setVendorAddressId(int vendorAddressId) {
+        this.vendorAddressId = vendorAddressId;
+    }
 
-	public void setVendorState(String vendorState) {
-		this.vendorState = vendorState;
-	}
+ 
 
-	public long getVendorPincode() {
-		return vendorPincode;
-	}
+    public String getVendorCity() {
+        return vendorCity;
+    }
 
-	public void setVendorPincode(long vendorPincode) {
-		this.vendorPincode = vendorPincode;
-	}
+ 
 
-	@Override
-	public String toString() {
-		return "Vendor_Address [vendorAddressId=" + vendorAddressId + ", vendorCity=" + vendorCity + ", vendorState="
-				+ vendorState + ", vendorPincode=" + vendorPincode + "]";
-	}
+    public void setVendorCity(String vendorCity) {
+        this.vendorCity = vendorCity;
+    }
+
+ 
+
+    public String getVendorState() {
+        return vendorState;
+    }
+
+ 
+
+    public void setVendorState(String vendorState) {
+        this.vendorState = vendorState;
+    }
+
+ 
+
+    public long getVendorPincode() {
+        return vendorPincode;
+    }
+
+ 
+
+    public void setVendorPincode(long vendorPincode) {
+        this.vendorPincode = vendorPincode;
+    }
+
+ 
+
+    @Override
+    public String toString() {
+        return "Vendor_Address [vendorAddressId=" + vendorAddressId + ", vendorCity=" + vendorCity + ", vendorState="
+                + vendorState + ", vendorPincode=" + vendorPincode + "]";
+    }
+
+ 
 
 }
